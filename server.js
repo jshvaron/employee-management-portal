74ae09b74e0ec531fe5b6521d4f0230d5b47e101
jshvaron <jshva@icloud.com>
2023-04-03 21:39:16 -0500

    added express dependency

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express;
+
+//Express mid ware
+app.request(express.urlencoded({ extended: false}));
+app.use(express.json());
