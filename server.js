706d9081dd534da5b93df15ebf1ce50db9e6d4c4
jshvaron <jshva@icloud.com>
2023-04-04 10:39:29 -0500

    server is up and running

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
 
 const PORT = process.env.PORT || 3001;
-const app = express;
+const app = express();
 
-//Express mid ware
-app.request(express.urlencoded({ extended: false}));
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to db
+const db = mysql.createConnection(
+    {
+        host: "127.0.0.1",//localhost did not work, why?
+        user: "root",
+        password: "root",
+        database: "departments_db",
+    },
+    console.log('connected to database')
+);
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+  });
