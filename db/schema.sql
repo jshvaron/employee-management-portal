a011ad2a951452cc6143866540b81b0618763af1
jshvaron <jshva@icloud.com>
2023-04-04 15:34:32 -0500

    deleted seeds file an cleaned up CREATE TABLES

--- db/schema.sql
+++ db/schema.sql
@@ -3,26 +3,33 @@ CREATE DATABASE departments_db;
 
 USE departments_db;
 
-CREATE TABLE departments(
-    department_name VARCHAR(40) NOT NULL,
-    id INT NOT NULL,
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE roles(
-    job_title VARCHAR(30),
-    role_id INT NOT NULL,
-    department_id INT NOT NULL,
-    salary INT NOT NULL
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
 );
 
-CREATE TABLE employees(
-    employee_id INT NOT NULL,
-    first_name VARCHAR(25),
-    last_name VARCHAR(25),
-    job_title VARCHAR(30),
-    department VARCHAR(50),
-    manager VARCHAR(50)
-
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
 );
 
 
