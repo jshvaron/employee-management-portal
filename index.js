59262521c5a174664433666fcae4a5d46bf2f071
jshvaron <jshva@icloud.com>
2023-04-06 10:24:28 -0500

    set switch case for main menu

--- index.js
+++ index.js
@@ -6,22 +6,49 @@ function startPortal(){
     function menu(){
         inquirer.prompt([{
 
-            name:   'Employee Managmemt Portal',
+            name:   'employeeManagmemtPortal',
             type: 'list',
             message: 'Welcome to Employee Management Portal, how can I assist?',
             choices: [
-                'View all Employees',
-                'Add Employee',
-                'Update Employee Role',
-                'View all Roles',
-                'Add Role',
                 'View all Departments',
                 'Add Departments',
+                'View all Roles',
+                'Add Role',
+                'Update Employee Role',
+                'View all Employees',
+                'Add Employee',
                 'Complete'
             ]
     
-    
-        }]).then(console.log('XXXXXXXXXXX'))
+        }]).then(async(answer)=> {
+            switch (answer.employeeManagmemtPortal) {
+                case 'View all Departments':
+
+                    break;
+                case 'Add Departments':
+
+                    break;
+                case 'View all Roles':
+
+                    break;
+                case 'Add Role':
+
+                    break;
+                case 'Update Employee Role':
+
+                    break;
+                case 'View all Employees':
+
+                    break;
+                case 'Add Employee':
+
+                    break;
+                case 'Complete' :
+
+                    break;
+            }
+
+        })
     }
     menu();
 
